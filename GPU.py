35d58f7c570bc9c7450893c63d7f47410f3564d9
IANNXANG <yx17180324@gmail.com>
2024-10-16 22:59:40 +0800

    output

--- GPU.py
+++ GPU.py
@@ -15,6 +15,8 @@ inputs = tokenizer(prompt, return_tensors="pt").to(device)
 
 # 生成回答
 outputs = model.generate(**inputs, max_length=1000)
+print(outputs[0])
+print(outputs)
 answer = tokenizer.decode(outputs[0], skip_special_tokens=False)
 
 print(answer)
