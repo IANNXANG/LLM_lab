b3b8dae38b3ecc5f159f42dfc104c5f2b76270ec
IANNXANG <yx17180324@gmail.com>
2024-10-16 22:12:12 +0800

    Qwen pipeline

---
+++ Qwen2.5.py
@@ -0,0 +1,8 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+messages = [
+    {"role": "user", "content": "What is the greatest common factor of $20 !$ and $200,\\!000$?  (Reminder: If $n$ is a positive integer, then $n!$ stands for the product $1\\cdot 2\\cdot 3\\cdot \\cdots \\cdot (n-1)\\cdot n$.)"},
+]
+pipe = pipeline("text-generation", model="Qwen/Qwen2-Math-7B-Instruct")
+pipe(messages)
