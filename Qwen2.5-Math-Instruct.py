e966987e265b914c3d9518417d1eee1e28d3159e
IANNXANG <yx17180324@gmail.com>
2024-10-17 15:38:21 +0800

    格式化

--- Qwen2.5-Math-Instruct.py
+++ Qwen2.5-Math-Instruct.py
@@ -26,8 +26,7 @@ model.to(device)
 
 def PrintQandA(prompt):
     # 可以添加一些示例输入进行测试
-    prompt = ("某人花19快钱买了个玩具，20快钱卖出去。他觉得不划算，又花21快钱买进，22快钱卖出去。"
-              "请问它赚了多少钱？\n\n")
+
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
 
     # 生成回答
@@ -36,7 +35,7 @@ def PrintQandA(prompt):
     # print(outputs)
     answer = tokenizer.decode(outputs[0], skip_special_tokens=False)
 
-    print(answer)
+
     print("------------------------------------------")
 
     parts = answer.split("\n\n")
