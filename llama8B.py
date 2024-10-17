e76264eb5d89ba874a15e69221aae9e3ede0da77
IANNXANG <yx17180324@gmail.com>
2024-10-17 12:50:06 +0800

    llama3

--- llama8B.py
+++ llama8B.py
@@ -6,15 +6,21 @@ path_to_model_directory = "/pubshare/LLM/Meta-Llama-3-8B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(path_to_model_directory)
 model = AutoModelForCausalLM.from_pretrained(path_to_model_directory)
 
+# 设置pad_token_id为eos_token_id
+model.config.pad_token_id = model.config.eos_token_id
+
 # 准备输入文本
 input_text = "你好，LLaMA！"
 
-# 将文本转为token
-inputs = tokenizer(input_text, return_tensors="pt")
+# 将文本转为token并生成attention_mask
+inputs = tokenizer(input_text, return_tensors="pt", padding=True)
+
+# 显式设置attention_mask
+attention_mask = inputs.attention_mask
 
 # 推理生成
-outputs = model.generate(inputs["input_ids"], max_length=500, num_return_sequences=1)
+outputs = model.generate(inputs["input_ids"], attention_mask=attention_mask, max_length=50, num_return_sequences=1)
 
 # 解码输出
-generated_text = tokenizer.decode(outputs[0], skip_special_tokens=False)
+generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 print(generated_text)
